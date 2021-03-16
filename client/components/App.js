827db6fd11239f71157d85c0403efd2cd8a6ee5d
Likhita Manchikanti <likhitamanchikanti@Likhitas-MacBook-Pro-2.local>
2021-03-16 15:42:53 -0400

    static homepage

--- client/components/App.js
+++ client/components/App.js
@@ -2,12 +2,13 @@ import '../css/App.css';
 
 //client/components/App.js
 import React, { Component } from 'react';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Daydream Magazine</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
